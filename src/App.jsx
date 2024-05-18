a8af490861504adca568b845b6d844b3c3c99467
Holovko Maksym <golovkomaksim852@gmail.com>
2024-05-18 12:54:22 +0300

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import Details from "./pages/MovieDetailsPage/MovieDetailsPage";
+import NotFound from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
-  return;
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="details">Details</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="details" element={<Details />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from "./App.jsx";
 
 import "modern-normalize";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
