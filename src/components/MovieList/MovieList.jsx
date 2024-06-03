a668a369e1aed050b93f5878549fdd8af510bbca
Holovko Maksym <golovkomaksim852@gmail.com>
2024-06-03 20:55:48 +0300

    img

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,24 @@
 import { Link, useLocation } from "react-router-dom";
+import css from "./MovieList.module.css";
 
 export default function MovieList({ movies }) {
   const location = useLocation();
 
   return (
     <div>
-      <ul>
+      <ul className={css.list}>
         {movies.map((movie) => (
           <li key={movie.id}>
             <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+              <img
+                src={
+                  movie.poster_path
+                    ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+                    : `http://www.suryalaya.org/images/no_image.jpg`
+                }
+                loading="lazy"
+                alt="Movie poster"
+              />
               <p>{movie.title}</p>
             </Link>
           </li>
